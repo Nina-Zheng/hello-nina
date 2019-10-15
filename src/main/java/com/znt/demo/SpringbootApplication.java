fe0fe0758fe719a990084c242df5e01815391c94
DWB-571-00430\Administrator <zhengniting@dianwoda.com>
2019-10-15 16:41:41 +0800

    IC-test

---
+++ src/main/java/com/znt/demo/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.znt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author  znt
+ * @date  2019/10/15
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
