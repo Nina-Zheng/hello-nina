85814490db3739575d4280e0bade053982410f4c
DWB-571-00430\Administrator <zhengniting@dianwoda.com>
2019-10-18 14:31:55 +0800

    fix

---
+++ src/main/java/com/znt/demo/TestController.java
@@ -0,0 +1,30 @@
+package com.znt.demo;
+
+import com.znt.demo.mail.MailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author znt
+ * @date  2019/10/18
+ */
+
+@Controller
+public class TestController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private MailService mailService;
+
+
+    @RequestMapping("/")
+    public void test() {
+
+        mailService.sendSimpleMail("zhengniting@dianwoda.com", "这是一封简单邮件", "大家好，这是我的第一封邮件！");
+
+    }
+
+}
