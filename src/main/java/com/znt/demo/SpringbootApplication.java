68bf700c2a39cc584d1474ba01b92d0135a02814
zhengniting <zhengniting@dianwoda.com>
2019-12-27 09:36:40 +0800

    添加日志定时清理

--- src/main/java/com/znt/demo/SpringbootApplication.java
+++ src/main/java/com/znt/demo/SpringbootApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class SpringbootApplication {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
 
         SpringApplication.run(SpringbootApplication.class, args);
         new Clock().loopReport();
